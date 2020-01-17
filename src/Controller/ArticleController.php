70737047e139ee090d99df7cca0a8492c970f007
NikoSpasic <nikola@spasic.dev>
2020-01-17 20:09:43 +0100

    Symfony_01 Stellar END

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,22 +4,22 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('WOWOEOWEIFOJYEFLJELWF');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -29,9 +29,24 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
+//        dump($slug, $this);
+
         return $this->render('article/show.html.twig', [
            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleheart($slug, LoggerInterface $logger)
+    {
+//        TODO -actually heart/unheart the article! DB
+
+        $logger->info('Article is being hearted');
+//        return new JsonResponse(['hearts' => rand(5, 100)]);
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
